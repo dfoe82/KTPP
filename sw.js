397a4b6041d6c4a23d3bbfacb1759d2e46bac234
dfoe82 <dennis.foelsch@t-online.de>
2025-06-28 00:39:16 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = 'anw-liste-v1';
+const FILES = [
+  '/',
+  '/index.html',
+  '/list.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+self.addEventListener('install', e =>
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)))
+);
+self.addEventListener('fetch', e =>
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))
+);
